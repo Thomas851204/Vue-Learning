0bdce6fdaf807715421f8a4d552bfcdedf91d4ee
Olasz Tamás <tamas.olasz1@gmail.com>
2023-11-21 14:02:18 +0100

    Refined Get calls HTML@CSS, created barebones for Post on FE

---
+++ Frontend/env.ts
@@ -0,0 +1,3 @@
+export class FEenv {
+  devbaseurl: string = "http://localhost:3000/";
+}
--- Frontend/src/views/HTTPcall.vue
+++ Frontend/src/views/HTTPcall.vue
@@ -4,35 +4,57 @@
     <div class="get">
       <p>Get</p>
       <button type="submit" @click="getClick">Get all users</button>
-      <div v-if="getCalled">
-        <p>{{ usersList }}</p>
+      <div class="getList" v-if="usersList">
+        <div class="column">
+          <p v-for="user in usersList" :key="user.id">{{ user.username }}</p>
+        </div>
+        <div class="column">
+          <p v-for="user in usersList" :key="user.id">{{ user.email }}</p>
+        </div>
       </div>
+      <button class="clear" @click="usersList = []">Clear</button>
     </div>
-    <button class="clear" @click="(getCalled = false), (usersList = [])">
-      Clear
-    </button>
+    <div class="post">
+      <p>Post</p>
+      <form class="postForm" @submit.prevent="postClick">
+        <label for="username">Username</label>
+        <input
+          type="text"
+          autocomplete="off"
+          name="username"
+          v-model="username"
+        />
+        <label for="email">Email</label>
+        <input type="email" autocomplete="off" name="email" v-model="email" />
+        <input type="submit" value="Post Data" />
+      </form>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import axios from "axios";
-import { DefineComponent, defineComponent } from "vue";
+import { defineComponent } from "vue";
+import { UserInterface } from "../models/User.interface";
+import { FEenv } from "../../env";
 export default defineComponent({
   name: "HTTPcall",
   data() {
     return {
       redir: "HTTP call testing",
-      getCalled: false,
-      usersList: [],
+      usersList: [] as UserInterface[],
+      username: "",
+      email: "",
+      devurl: new FEenv(),
     };
   },
   methods: {
     async getClick() {
-      this.getCalled = true;
       this.usersList = (
-        await axios.get("http://localhost:3000/users/getAll")
+        await axios.get(this.devurl.devbaseurl + "users/getAll")
       ).data;
     },
+    async postClick() {},
   },
 });
 </script>
@@ -41,5 +63,18 @@ export default defineComponent({
 .body {
   height: 100vh;
   margin-left: 10px;
+}
+.getList {
+  display: flex;
+}
+
+.column {
+  flex: 1;
+  max-width: 100px;
+}
+form,
+label,
+input {
+  display: block;
 }
 </style>
