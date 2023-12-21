94fdebdf38b068fdb845cde7e3527dca7b8f932c
Olasz Tamás <tamas.olasz1@gmail.com>
2023-12-21 16:31:02 +0100

    Packed views into dropdown menu

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="nav">
     <router-link to="/" class="navIcon">Home</router-link>
-    <router-link to="/redir" class="navIcon">Redirected</router-link>
-    <router-link to="/http" class="navIcon">HTTP test</router-link>
-    <router-link to="/form" class="navIcon">Form</router-link>
+    <div class="dropdown" @mouseleave="resetDropdown">
+      <div class="navIcon" @mouseenter="showDropdown">Tests</div>
+      <div class="contentWrap" v-if="selectedTest">
+        <router-link to="/redir" class="dd" @click="resetDropdown"
+          >Redirected</router-link
+        >
+        <router-link to="/http" class="dd" @click="resetDropdown"
+          >HTTP calls</router-link
+        >
+        <router-link to="/form" class="dd" @click="resetDropdown"
+          >Form tests</router-link
+        >
+      </div>
+    </div>
     <router-link to="/mine" class="navIcon">Minesweeper</router-link>
   </div>
   <router-view />
@@ -13,6 +24,19 @@
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "App",
+  data() {
+    return {
+      selectedTest: false,
+    };
+  },
+  methods: {
+    showDropdown() {
+      this.selectedTest = true;
+    },
+    resetDropdown() {
+      this.selectedTest = false;
+    },
+  },
 });
 </script>
 
@@ -33,8 +57,26 @@ export default defineComponent({
   align-items: center;
 }
 .navIcon {
+  border: none;
+  background-color: black;
   color: white;
   text-decoration: none;
   padding-left: 20px;
+  margin-bottom: 10px;
+}
+.contentWrap {
+  position: absolute;
+  background-color: black;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  width: 80px;
+  align-items: center;
+}
+.dd {
+  font-size: smaller;
+  background-color: black;
+  color: white;
+  margin-bottom: 10px;
 }
 </style>
