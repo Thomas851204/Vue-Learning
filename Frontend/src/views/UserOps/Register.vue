8ccc4dbf58665192be554053f7e81519f88c0db1
Olasz Tamás <tamas.olasz1@gmail.com>
2024-01-02 18:11:07 +0100

    vue version cleanup, vuelidator inst, created input fields and some validations

--- Frontend/src/views/UserOps/Register.vue
+++ Frontend/src/views/UserOps/Register.vue
@@ -1,25 +1,37 @@
 <template>
   <div class="body">
     <div class="cardWrap">
-      <div class="formWrap" @submit.prevent="regFormPost">
+      <form class="formWrap" @submit.prevent="regFormPost">
         <label for="username">Username</label>
         <input
           type="text"
           autocomplete="off"
           name="username"
-          v-model="username"
-          required
-          maxlength="10"
+          v-model="state.username"
         />
+        <span v-if="v$.username.$error">{{
+          v$.username.$errors[0].$message
+        }}</span>
         <label for="email">Email</label>
         <input
           type="email"
           autocomplete="off"
           name="email"
-          v-model="email"
-          required
+          v-model="state.email"
         />
-      </div>
+        <span v-if="v$.email.$error">{{ v$.email.$errors[0].$message }}</span>
+        <label for="password">Password</label>
+        <input
+          type="password"
+          autocomplete="off"
+          name="password"
+          v-model="state.password"
+        />
+        <span v-if="v$.password.$error">{{
+          v$.password.$errors[0].$message
+        }}</span>
+        <input type="submit" value="Register" />
+      </form>
       <router-link to="/login"
         >Already have an account? Sign in here!</router-link
       >
@@ -31,16 +43,46 @@
 import { defineComponent } from "vue";
 import axios from "axios";
 import { FEenv } from "@/env";
+import { reactive } from "vue";
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, minLength } from "@vuelidate/validators";
+
 export default defineComponent({
   name: "Register",
-  data() {
-    return {
+  setup() {
+    const state = reactive({
       username: "",
       email: "",
+      password: "",
+    });
+
+    function pwPattern(value: string): boolean {
+      const pat =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+      return pat.test(value);
+    }
+    const rules = {
+      username: { required },
+      email: { required, email },
+      password: { required, minLength: minLength(8), pwPattern },
     };
+
+    const v$ = useVuelidate(rules, state);
+
+    return { state, v$ };
+  },
+  data() {
+    return {};
   },
   methods: {
-    regFormPost() {},
+    regFormPost() {
+      this.v$.$validate();
+      if (this.v$.$invalid) {
+        alert("FAIL");
+      } else {
+        alert("success");
+      }
+    },
   },
 });
 </script>
@@ -56,10 +98,16 @@ export default defineComponent({
   align-items: center;
   justify-content: center;
   margin-top: 15%;
+  width: 25%;
+  height: 50%;
+  border: solid 1px black;
 }
 .formWrap {
   display: flex;
   flex-direction: column;
   text-decoration: none;
+}
+input {
+  width: fit-content;
 }
 </style>
